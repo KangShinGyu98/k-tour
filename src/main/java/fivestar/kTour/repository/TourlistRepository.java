b56690430d8d127ac974ae83e251865dd0239d0c
Shingyu Kang <rkdtlseb@naver.com>
2023-08-16 18:41:20 +0900

    [refactor] change name of TourlistRepository

--- src/main/java/fivestar/kTour/repository/TourlistRepository.java
+++ src/main/java/fivestar/kTour/repository/TourlistRepository.java
@@ -1,2 +1,7 @@
-package fivestar.kTour.repository;public class tourListRepository {
+package fivestar.kTour.repository;
+
+import fivestar.kTour.domain.Tourlist;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TourlistRepository extends JpaRepository<Tourlist,Long> {
 }
