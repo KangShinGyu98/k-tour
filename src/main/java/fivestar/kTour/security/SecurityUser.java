12a739daffdc54bec7f4d6ef88962f2690b6d370
soonhankwon <soonable@gmail.com>
2023-08-30 16:07:41 +0900

    feat : UserDetails 구현 (delegation)

---
+++ src/main/java/fivestar/kTour/security/SecurityUser.java
@@ -0,0 +1,55 @@
+package fivestar.kTour.security;
+
+import fivestar.kTour.domain.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+@RequiredArgsConstructor
+public class SecurityUser implements UserDetails {
+
+    private final User user;
+
+    @Override
+    public String getUsername() {
+        return user.getUserEmail();
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Arrays.stream(user.getRole().split(","))
+                .map(SimpleGrantedAuthority::new)
+                .toList();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+
+}
