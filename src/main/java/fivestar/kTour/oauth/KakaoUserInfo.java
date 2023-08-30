cf83980717b88905845fa6756e56d8663eee216b
soonhankwon <soonable@gmail.com>
2023-08-30 17:23:19 +0900

    feat : 유저 프로필 항목들 매핑 메서드들 (Kakao 에서 응답받은값)

--- src/main/java/fivestar/kTour/oauth/KakaoUserInfo.java
+++ src/main/java/fivestar/kTour/oauth/KakaoUserInfo.java
@@ -25,7 +25,27 @@ public class KakaoUserInfo implements Oauth2UserInfo {
         return (String) getKakaoAccount().get("email");
     }
 
+    @Override
+    public String getNickName() {
+        return (String) getProfile().get("nickname");
+    }
+
+    @Override
+    public String getImageUrl() {
+        return (String) getProfile().get("profile_image_url");
+    }
+
+    @Override
+    public Integer getAgeRange() {
+        String ageRange = (String) getKakaoAccount().get("age_range");
+        return Integer.parseInt(ageRange.split("~")[0]);
+    }
+
     public Map<String, Object> getKakaoAccount() {
         return (Map<String, Object>) attributes.get("kakao_account");
+    }
+
+    public Map<String, Object> getProfile() {
+        return (Map<String, Object>) getKakaoAccount().get("profile");
     }
 }
