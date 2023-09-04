3542e9021605c1027342c54097e1a2e08b0455af
soonhankwon <soonable@gmail.com>
2023-09-04 15:24:53 +0900

    feat : 네이버 OauthService 제네릭으로 선언 및 사용 <NaverOauthLoginParam>, login 메서드에서 해당 객체 사용하도록

---
+++ src/main/java/fivestar/kTour/service/NaverOauthService.java
@@ -0,0 +1,126 @@
+package fivestar.kTour.service;
+
+import fivestar.kTour.Dto.LoginResponseDto;
+import fivestar.kTour.Dto.OauthTokenResponseDto;
+import fivestar.kTour.domain.User;
+import fivestar.kTour.oauth.NaverOauthLoginParam;
+import fivestar.kTour.oauth.NaverUserInfo;
+import fivestar.kTour.oauth.Oauth2UserInfo;
+import fivestar.kTour.repository.UserRepository;
+import fivestar.kTour.security.service.TokenService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.MediaType;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Collections;
+import java.util.Map;
+import java.util.Optional;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class NaverOauthService implements OauthService<NaverOauthLoginParam> {
+
+    private final UserRepository userRepository;
+    private final InMemoryClientRegistrationRepository inMemoryRepository;
+    private final TokenService tokenService;
+
+    /**
+     * @InMemoryRepository application-oauth properties 정보를 담고 있음
+     * @getToken() 넘겨받은 code 로 Oauth 서버에 Token 요청
+     * @getUserProfile 첫 로그인 시 회원 가입
+     * 유저 인증 후 Jwt AccessToken 생성
+     */
+
+    @Transactional
+    @Override
+    public LoginResponseDto login(NaverOauthLoginParam params) {
+
+        ClientRegistration provider = inMemoryRepository.findByRegistrationId(params.provider());
+        log.info("redirect uri = {}", provider.getRedirectUri());
+        OauthTokenResponseDto tokenResponse = getToken(params.code(), provider, params.state());
+        log.info("OauthAccessToken = {}", tokenResponse.access_token());
+        User user = getUserProfile(params.provider(), tokenResponse, provider);
+
+        String accessToken = tokenService.generateToken(user.getUserEmail());
+        log.info("ServerAccessToken={}", accessToken);
+        return new LoginResponseDto(user.getUserEmail(), "BEARER_TYPE", accessToken);
+    }
+
+    private OauthTokenResponseDto getToken(String code, ClientRegistration provider, String state) {
+        return WebClient.create()
+                .post()
+                .uri(provider.getProviderDetails().getTokenUri())
+                .headers(header -> {
+                    header.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+                    header.setAcceptCharset(Collections.singletonList(StandardCharsets.UTF_8));
+                })
+                .bodyValue(requestToken(code, provider, state))
+                .retrieve()
+                .bodyToMono(OauthTokenResponseDto.class)
+                .block();
+    }
+
+    private MultiValueMap<String, String> requestToken(String code, ClientRegistration provider, String state) {
+        MultiValueMap<String, String> formData = new LinkedMultiValueMap<>();
+        formData.add("code", code);
+        formData.add("grant_type", "authorization_code");
+        formData.add("redirect_uri", provider.getRedirectUri());
+        formData.add("client_secret", provider.getClientSecret());
+        formData.add("client_id", provider.getClientId());
+        formData.add("state", state);
+        return formData;
+    }
+
+    private User getUserProfile(String providerName, OauthTokenResponseDto tokenResponse, ClientRegistration provider) {
+        Map<String, Object> userAttributes = getUserAttribute(provider, tokenResponse);
+        if (!providerName.equals("naver")) {
+            throw new RuntimeException("invalid provider name");
+        }
+
+        Oauth2UserInfo oauth2UserInfo = new NaverUserInfo(userAttributes);
+        if (oauth2UserInfo.getProvider() == null) {
+            throw new RuntimeException("provider is null");
+        }
+
+        String email = oauth2UserInfo.getEmail();
+        String nickname = oauth2UserInfo.getNickName();
+        String imageUrl = oauth2UserInfo.getImageUrl();
+        String ageRange = oauth2UserInfo.getAgeRange();
+        String oauth2Provider = oauth2UserInfo.getProvider();
+        String providerId = oauth2UserInfo.getProviderId();
+
+        log.info("ageRange={}", ageRange);
+        log.info("imageUrl={}", imageUrl);
+
+        Optional<User> optionalUser = userRepository.findById(email);
+
+        if (optionalUser.isEmpty()) {
+            User user = new User(email, nickname, imageUrl, ageRange, oauth2Provider, providerId);
+            userRepository.save(user);
+            return user;
+        } else {
+            return optionalUser.get();
+        }
+    }
+
+    private Map<String, Object> getUserAttribute(ClientRegistration provider, OauthTokenResponseDto tokenResponse) {
+        return WebClient.create()
+                .get()
+                .uri(provider.getProviderDetails().getUserInfoEndpoint().getUri())
+                .headers(header -> header.setBearerAuth(String.valueOf(tokenResponse.access_token())))
+                .retrieve()
+                .bodyToMono(new ParameterizedTypeReference<Map<String, Object>>() {
+                })
+                .block();
+    }
+}
